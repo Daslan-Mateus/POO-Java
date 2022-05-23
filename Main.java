e2b36fb56f5a48d7c0da37fd30e93412d0779c49
Daslan Mateus <70038813+Daslan-Mateus@users.noreply.github.com>
2022-05-23 18:47:55 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -2,13 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Robo r = new Robo();
         r.girarEsquerda(90);
-        r.girarEsquerda(180);
-        r.girarEsquerda(270);
-        r.girarEsquerda(380);
+        r.girarDireita(180);
+        r.passoFrente(270);
+        r.passoTras(380);
         r.girarEsquerda(90);
-        r.girarEsquerda(180);
-        r.girarEsquerda(270);
-        r.girarEsquerda(380);
+        r.girarDireita(180);
+        r.passoFrente(270);
+        r.passoTras(380);
     }
     
 }
--- Robo.java
+++ Robo.java
@@ -19,17 +19,44 @@ if(carga <= 20){
 System.out.println();
 }
 
-public void girarDireita(){
+public void girarDireita(int xD){
     System.out.println("Seu Robô deu passos para a direita");
+    if(xD % 2 == 0 ){
+        carga = carga -10;
+        System.out.println("sua bateria tem carga de "+carga + "%");
+        System.out.println("Sorria para a foto ");
+    }
+    if(carga <= 20){
+        System.out.println("sua bateria precisa ser recarre");
+    }
+    System.out.println();
 }
 
-public void passoFrente() {
+public void passoFrente(int xF) {
     
     System.out.println("Seu Robô deu passos para frente");
+    if(xF % 2 == 0 ){
+        carga = carga -10;
+        System.out.println("sua bateria tem carga de "+carga + "%");
+        System.out.println("Sorria para a foto ");
+    }
+    if(carga <= 20){
+        System.out.println("sua bateria precisa ser recarre");
+    }
+    System.out.println();
 }
 
-public void passoTras() {
+public void passoTras(int xT) {
     System.out.println("Seu Robô deu passos para trás");
+    if(xT % 2 == 0 ){
+        carga = carga -10;
+        System.out.println("sua bateria tem carga de "+carga + "%");
+        System.out.println("Sorria para a foto ");
+    }
+    if(carga <= 20){
+        System.out.println("sua bateria precisa ser recarregada");
+    }
+    System.out.println();
 }
 
 
